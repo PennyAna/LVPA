888b11ffd9f3f0307fea8bb07ffa33f9f6492f25
PennyAna <law07006@byui.edu>
2020-12-05 22:17:33 -0700

    added new table display to server/db ejs

--- server.js
+++ server.js
@@ -16,7 +16,27 @@ app.get('/postage', function (req, res) {
 app.get('/db', async function(req, res) {
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
+        const result = await client.query('SELECT * FROM media_table');
+        const results = { 'results': (result) ? result.rows: null};
+        res.render('pages/db.ejs', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM genre_table');
+        const results = { 'results': (result) ? result.rows: null};
+        res.render('pages/db.ejs', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM login_table');
         const results = { 'results': (result) ? result.rows: null};
         res.render('pages/db.ejs', results);
         client.release();
