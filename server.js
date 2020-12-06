df2d9f0f2dcd5490095de7baf52bd8ae907e92f0
PennyAna <law07006@byui.edu>
2020-12-05 22:21:47 -0700

    added new table display to server/db ejs

--- server.js
+++ server.js
@@ -16,28 +16,12 @@ app.get('/postage', function (req, res) {
 app.get('/db', async function(req, res) {
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT * FROM media_table');
-        const results = { 'results': (result) ? result.rows: null};
-        res.render('pages/db.ejs', results);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM genre_table');
-        const results = { 'results': (result) ? result.rows: null};
-        res.render('pages/db.ejs', results);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM login_table');
-        const results = { 'results': (result) ? result.rows: null};
+        const result1 = await client.query('SELECT * FROM media_table');
+        const results1 = { 'results1': (result1) ? result1.rows: null};
+        const result2 = await client.query('SELECT * FROM genre_table');
+        const results2 = { 'results2': (result2) ? result2.rows: null};
+        const result3 = await client.query('SELECT * FROM login_table');
+        const results3 = { 'results3': (result3) ? result3.rows: null};
         res.render('pages/db.ejs', results);
         client.release();
     } catch (err) {
