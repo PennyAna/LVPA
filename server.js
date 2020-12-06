183c4e7d462390746c90b90f36b7d77b45785b77
PennyAna <law07006@byui.edu>
2020-12-05 22:37:18 -0700

    and again

--- server.js
+++ server.js
@@ -16,13 +16,9 @@ app.get('/postage', function (req, res) {
 app.get('/db', async function(req, res) {
     try {
         const client = await pool.connect();
-        const result1 = await client.query('SELECT * FROM media_table');
-        const results1 = { 'results1': (result1) ? result1.rows: null};
-        const result2 = await client.query('SELECT * FROM genre_table');
-        const results2 = { 'results2': (result2) ? result2.rows: null};
-        const result3 = await client.query('SELECT * FROM login_table');
-        const results3 = { 'results3': (result3) ? result3.rows: null};
-        res.render('pages/db.ejs', results1, results2, results3);
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows: null};
+        res.render('pages/db.ejs', results);
         client.release();
     } catch (err) {
         console.error(err);
