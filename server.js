da2afb513a4b6bff5af13362dfb8e803888321a8
PennyAna <law07006@byui.edu>
2020-12-06 19:22:08 -0700

    git repo

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 var express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000
+
 var flash = require('connect-flash');
-var passport = require('passport');
+
+var passport = require("passport");
 var request = require('request');
-var session = require('express-session');
+
+var session = require("express-session");
+
 var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, '/')));
-app.set('port', (process.env.PORT || 5000));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
 
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-const expressSession = require('express-session');
-app.use(expressSession({secret: 'mySecretKey'}));
+
+
 app.use(passport.initialize());
 app.use(passport.session());
+
+var bodyParser = require('body-parser')
+
+var path = require('path');
+
 app.use('/public', express.static(__dirname + '/public'));
+
 app.use(flash());
 app.use(session({secret: 'keyboard cat'}))
 app.use(bodyParser());
+app.set('view engine', 'pug');
+app.set('view options', { layout: false });
 
-require('./routes.js') (app);
+
+require('./lib/routes.js')(app);
+
+app.listen(PORT);
+console.log('Node listening on port %s', PORT);
